f8f393ccaa3e9bdbd37d3fdd3b3d4554a978b5e5
Jerry <gerardofv02@gmail.com>
2023-02-12 20:46:51 +0100

    Updated

--- RickYmortyEjercicio/functions.js
+++ RickYmortyEjercicio/functions.js
@@ -24,13 +24,10 @@ function fetchCharsp(page) {
             const name = document.createElement("div");
             let x = document.createTextNode(`${char.name}`);
             name.appendChild(x);
-
             container.appendChild(name);
             
-
             const img = document.createElement("img");
             img.src = `${char.image}`;
-
            container.appendChild(img);
            */
 
@@ -59,66 +56,31 @@ function fetchCharsn(name) {
             const name = document.createElement("div");
             let x = document.createTextNode(`${char.name}`);
             name.appendChild(x);
-
             container.appendChild(name);
             
-
             const img = document.createElement("img");
             img.src = `${char.image}`;
-
            container.appendChild(img);
            */
 
         }
     })
 }
-function fetchCharsp_n(page, name) {
-
-
-
-    fetch(`https://rickandmortyapi.com/api/character/?page=${page}&name=${name}`)
-    .then(data => data.json())
-    .then(chars => {
-        var container = document.getElementById("results");
-        container.innerHTML = "";
-        console.log(chars, "\n");
-        for (var char of chars.results) {
-
-            var div = document.createElement("div");
-            div.innerHTML = `
-                <h3>${char.name}</h3>
-                <img src="${char.image}" alt="${char.name}">
-            `;
-            container.appendChild(div);
-            /*
-            const name = document.createElement("div");
-            let x = document.createTextNode(`${char.name}`);
-            name.appendChild(x);
-
-            container.appendChild(name);
-            
-
-            const img = document.createElement("img");
-            img.src = `${char.image}`;
-
-           container.appendChild(img);
-           */
-
-        }
-    })
-}
-function getPage(page){
-    var page = document.getElementById("page").value;
+function getPage(){
+    console.log(page);
+     page = parseInt(document.getElementById("page").value);
+     console.log(page);
     if(page <=1 && page > 41){
         window.alert("Maximo de 42 pags!");
     }else{
         fetchCharsp(page);
+        console.log(page);
     }
 
 }
 
 function getName(name){
-    var name = document.getElementById("nombre").value;
+     name = document.getElementById("name").value;
 
 
 
@@ -128,7 +90,7 @@ function getName(name){
 }
 
 function anterior(){
-    if(page <= 1){
+    if(page < 1){
 
     }
     else{
@@ -137,11 +99,14 @@ function anterior(){
     }
 }
 function siguiente(){
-    if(page > 41){
+    console.log(page);
+    if(page > 42){
 
     }else{
-    page = page+1;
+    page = page + 1;
+    console.log(page);
     fetchCharsp(page);
+    console.log(page);
     }
 
 }
